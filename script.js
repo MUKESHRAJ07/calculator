f145576187ed6bde854d969b004855ab0273041a
Mukesh raj <79533154+MUKESHRAJ07@users.noreply.github.com>
2021-06-08 13:47:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//function to calculate interest
+function compute()
+{
+    //to get input value from user
+    var principal = document.getElementById("principal").value;
+    var rate=document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    //validation for positive value
+    if(principal<=0)
+    {
+alert("Enter a positive number");
+return;
+    }
+    else
+    {
+    document.getElementById("result").innerHTML="If you deposit "+principal+"<br> at an interest rate of "+rate+"%.<br> You will receive an amount of "+interest+",<br> in the year "+year;
+    }
+}
+//function to update range value
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+         
